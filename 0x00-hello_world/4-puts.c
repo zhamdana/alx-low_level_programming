4078c83c454651f397193d96f75631f982658f89
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-13 21:30:14 +0000

    Amendment of 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-int main(void) /*main function allows for use of puts function */
+/**
+*main - this function allows for use of puts.
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
