16834df655f6660e7c6487a2bfa3e55e42a7f668
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-13 23:01:49 +0000

    Yes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - this function allows for use of printf.
+*Return: ensures main returns to 1.
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
