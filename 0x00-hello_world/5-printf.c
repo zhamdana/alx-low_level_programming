f889934f36a88567b5370ae7358a848fdd043fb0
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-13 21:40:18 +0000

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - this function allows for use of printf.
+*Return: ensures main returns to 0.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
