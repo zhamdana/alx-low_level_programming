a1fa09df63fc4981f1ebf2473b750f19e9edc7e8
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-14 19:38:51 +0000

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - print single digit numbers of base 10 starting from 0 with putchar
+*Return: 0 if successful
+*/
+int main(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		putchar(j);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
