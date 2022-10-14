5d612f652a71f433da333e105ab2b1e1569cfc57
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-14 16:27:19 +0000

    2. I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - determines if number is positive or negative
+*Return: 0 if successful
+*/
+int main(void)
+{
+	int j;
+	char ch = 'a';
+
+	j = 0;
+
+	while (j != 26)
+	{
+		putchar (ch);
+		ch++;
+		j++;
+		if (j == 26)
+		putchar ('\n)
+	}
+	return (0);
+}
