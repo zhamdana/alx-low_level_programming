991face1a6e31b14e3dd1d3d5322464b62d2a7ea
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-14 19:49:54 +0000

    Print numbers with spaceand comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - print numbers from 0 to 9 with comma and space
+*Return: 0 if successful
+*/
+int main(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		putchar(j);
+		if (j !=9)
+		{	
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
