5f310371c59534a352564a656a900bdc38c6eeff
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-18 19:51:33 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * main - prints _putchar followed by newline
+  *Return: returns 0 when successful
+  */
+int main(void)
+{
+	int word[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int c, s;
+
+	s = sizeof(word) / sizeof(int);
+	for (c =0; c < s; c++)
+	{
+		_putchar(word[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Functions, loops, and nested loops folder
