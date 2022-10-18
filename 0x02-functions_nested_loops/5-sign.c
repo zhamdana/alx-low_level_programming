2cb59fa1b69f03deb246d8ee76ee40cffc521e61
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-18 21:52:29 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,15 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	if else(n == 0)
+	if else (n < 0)
 	{
-		_putchar('0');
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
 		_putchar('-');
-		return (-1);
+		return (0);
 	}
 	_putchar('\n');
 }
