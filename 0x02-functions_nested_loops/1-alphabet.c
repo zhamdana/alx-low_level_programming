18e791c0c3a2297facc5119b3e878bda4b01fd37
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-18 20:23:48 +0000

    Print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *void print_alphabet - Print alphabets in lowercase 
+  *Return: returns 0 when successful
+  */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 97; j <=122;)
+	j++;
+	_putchar('\n');
+}
