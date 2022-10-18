3a4d0e603e8c8358485f4086852d7d0b2a604eb3
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-18 21:30:26 +0000

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  *Return: returns 0 or 1 when successful
  *@c: the parameter to be used in the function
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c > 96 && c < 123) || (c > 64 && c < 91))
 		return (1);
