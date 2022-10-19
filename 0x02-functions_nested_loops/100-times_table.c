114185d5ac6aacb8fa73fdfd88a9ec627c076f4f
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-19 10:59:53 +0000

    Multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,27 +18,28 @@ int k;
 			_putchar('0');
 			for (j = 1; j <= n; j++)
 			{
-				k = i * j;
 				_putchar(',');
 				_putchar(' ');
+				k = i * j;
+
+				if (k <= 99)
+				{
+					_putchar(' ');
+				}
 				if (k <= 9)
 				{
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
 				}
-				else if (k <= 99)
+				if (k >= 100)
 				{
-					_putchar('0');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else
-				{
-					_putchar(((k / 100) % 10) + '0');
+					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
 				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar((k / 10) + '0');
+				}
+				_putchar((k % 10) + '0');
 			}
 			_putchar('\n');
 		}
