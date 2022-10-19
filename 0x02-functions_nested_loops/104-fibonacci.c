c44c586470732d37d83fda4af5967a4e9829ad5c
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-19 14:31:39 +0000

    Find and print the first 98 Finoncci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,18 +6,19 @@
  */
 int main(void)
 {
-	long double c, s, f1, f2;
+	int c;
+	long int s, f1, f2;
 
 	f1 = 1;
 	f2 = 2;
-	printf("%.LF, ", f1);
-	printf("%.LF, ", f2);
+	printf("%ld, ", f1);
+	printf("%ld, ", f2);
 	for (c = 0; c < 96; c++)
 	{
 		s = f1 + f2;
 		f1 = f2;
 		f2 = s;
-		printf("%.LF", s);
+		printf("%ld", s);
 		if (c != 95)
 		printf(", ");
 	}
