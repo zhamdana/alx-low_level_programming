e56ff1f2001871cc69e60cb5e7e36b1910a6c278
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-20 19:43:37 +0000

    Print numbers from0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+	}
+	_putchar('\n');
+}
