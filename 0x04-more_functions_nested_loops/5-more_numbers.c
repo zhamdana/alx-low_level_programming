cd5f736285610f033e24182025b2ddf35ced35ed
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-20 20:10:15 +0000

    Correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,15 @@
 void more_numbers(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i > 9)
-				_putchar((i / 10) + 48);
-			_putchar((i % 10) + 48);
+			if (j > 9)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
 		}
 	}
 	_putchar('\n');
