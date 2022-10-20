c84b3244da9498cbe928175cfba1e7c6df23f345
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-20 21:45:55 +0000

    Print numbers and buzz fizz in alternation

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print numbers and buzz
+ *Return: returns 0 if function works
+ */
+int main(void)
+{
+	int i = 1;
+
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else
+			printf(" %d", i);
+	}
+	printf("\n");
+	return (0);
+}
