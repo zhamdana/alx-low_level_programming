bb2bf4b83e438f10f97547a186998cad1f0cf8d1
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-20 21:10:00 +0000

    Print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,21 +9,20 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
+		_putchar('\n');
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			if (i == n - 1)
-				{
-				continue;
-				}
+			_putchar(' ');
+		}
+			if (n > 0)
+		{
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
