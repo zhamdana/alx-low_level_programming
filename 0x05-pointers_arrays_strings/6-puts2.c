e905e0c3cb4ba315b9a79e66cd9c9dd341b5d6b6
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-24 21:42:55 +0000

    Print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,10 @@ void puts2(char *str)
 	int i;
 	int j;
 
-	for (j = 0; str[j] != '\0'; j++)
+	for (j = 0; str[i++]; j++)
 	{
 	}
 	for (i = 0; i < j; i++)
-
 		_putchar(str[i]);
-	i++;
-
 	_putchar('\n');
 }
