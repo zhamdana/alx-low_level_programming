ba7e932c61af8ed1860d2006a6884277ed06805f
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-28 22:16:10 +0000

    Capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * *cap_string - capitalizes all words of a string
+  * @i: input string
+  * Return: return i
+  **/
+char *cap_string(char *i)
+{
+	int j;
+
+	for (j = 0; i[j] != '\0'; j++)
+	{
+	if (i[0] >= 97 && i[0] <= 122)
+	{
+		i[0] = i[0] - 32;
+		if (i[j] == 32 || i[j] == 9 ||
+		    i[j] == 10 || i[j] == 44 ||
+		    i[j] == 59 || i[j] == 46 ||
+		    i[j] == 33 || i[j] == 63 ||
+		    i[j] == 34 || i[j] == 40 ||
+		    i[j] == 41 || i[j] == 123 ||
+		    i[j] == 125)
+			i[j] -= 32;
+			if (i[j + 1] >= 97 && i[j + 1] <= 122)
+				i[j + 1] = i[j + 1] - 32;
+	}
+	}
+	return (i);
+}
