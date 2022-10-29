15d69a29394332ac665e6be9c6b446ac94f11e38
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-29 22:41:55 +0000

    Add two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,37 +1,44 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
-  * *infinite_add - function add two numbers
-  * @n1: number
-  * @n2: number
-  * @r: buffer for storing result
-  * @size_r: buffer size
-  * Return: return r if successful
-  **/
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a = 0, b = 0, sum = 0, i, j, k, l;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	for (; *(n1 + a) != '\0'; a++)
-	for (; *(n2 + b) != '\0'; b++)
-	if (a >= b)
-		j = a;
+	for (; *(n1 + c1) != '\0'; c1++;)
+	for (; *(n2 + c2) != '\0'; c2++;)
+	if (c1 >= c2)
+		bg = c1;
 	else
-		j = b;
-	if (size_r <= j + 1)
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-	r[j + 1] = '\0', a--, b--, size_r--, k = *(n1 + a) - 48, l = *(n2 + b) - 48;
-	for (; j >= 0; i = k + l + sum)
+	r[bg + 1] = '\0', c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		if (i >= 10 || a > 0)
-			sum = i / 10, a--, k = *(n1 + a) - 48;
+		op = dr1 + dr2 + add;
+		if ((op >= 10) || (c1 > 0))
+			add = op / 10, c1--, dr1 = *(n1 + c1) - 48;
 		else
-			sum = 0, k = 0;
-		if (i > 0 || b > 0)
-			*(r + j) = (i % 10) + 48, b--, l = *(n2 + b) - 48;
+			add = 0, dr1 = 0;
+		if ((op > 0) || (c2 > 0))
+		*(r + bg) = (op % 10) + 48, c2--, dr2 = *(n2 + c2) - 48;
 		else
-			*(r + j) = '0', l = 0;
-		j--, size_r--;
+			*(r + bg) = '0', dr2 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
