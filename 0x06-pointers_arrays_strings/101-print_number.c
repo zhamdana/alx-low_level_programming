359147f199672b448332c1b59c715a1faff2f179
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-29 21:10:58 +0000

    Print integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -21,9 +21,9 @@ void print_number(int n)
 	k = 1;
 	while (j > 9)
 	{
-		j = j /  10;
-		k = j * 10;
+		j /= 10;
+		k *= 10;
 	}
-	for (; k >= 1; k = / 10)
+	for (; k >= 1; k /= 10)
 		_putchar(((i / k) % 10) + 48);
 }
