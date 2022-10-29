83474400837bfc8501f50b7c6367ad5003f187bd
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-29 23:11:23 +0000

    Add numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,46 +6,46 @@
  * @n2: number
  * @r: buffer store result.
  * @size_r: buffer size:
- * Return: the pointer selected value
+ * Return: returns selected value
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int a = 0, b = 0, i, bg, j, k, c = 0;
+    int a = 0, b = 0, sum = 0, i, j, k, l;
 
     while (*(n1 + a) != '\0')
         a++;
     while (*(n2 + b) != '\0')
         b++;
     if (a >= b)
-        bg = a;
+        j = a;
     else
-        bg = b;
-    if (size_r <= bg + 1)
+        j = b;
+    if (size_r <= j + 1)
         return (0);
-    r[bg + 1] = '\0';
+    r[j + 1] = '\0';
     a--, b--, size_r--;
-    j = *(n1 + a) - 48, k = *(n2 + b) - 48;
-    while (bg >= 0)
+    k = *(n1 + a) - 48, l = *(n2 + b) - 48;
+    while (j >= 0)
     {
-        i = j + k + c;
+        i = k + l + sum;
         if (i >= 10)
-            c = i / 10;
+            sum = i / 10;
         else
-            c = 0;
+            sum = 0;
         if (i > 0)
-        *(r + bg) = (i % 10) + 48;
+        *(r + j) = (i % 10) + 48;
         else
-            *(r + bg) = '0';
+            *(r + j) = '0';
         if (a > 0)
-            a--, j = *(n1 + a) - 48;
-        else
-            j = 0;
-        if (b > 0)
-            b--, k = *(n2 + b) - 48;
+            a--, k = *(n1 + a) - 48;
         else
             k = 0;
-        bg--, size_r--;
+        if (b > 0)
+            b--, l = *(n2 + b) - 48;
+        else
+            l = 0;
+        j--, size_r--;
     }
     if (*(r) == '0')
         return (r + 1);
