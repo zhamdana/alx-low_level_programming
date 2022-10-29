588a785dc79864da1f8aaccb6ad0232b51886695
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-29 23:31:15 +0000

    Add numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,40 +11,40 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int a = 0, b = 0, sum = 0, i, j, k, l;
+	int a = 0, b = 0, sum = 0, i, j, k, l;
 
-    while ((*(n1 + a) != '\0') && (*(n2 + b) != '\0'))
-        a++, b++;
-    if (a >= b)
-        j = a;
-    else
-        j = b;
-    if (size_r <= j + 1)
-        return (0);
-    r[j + 1] = '\0', a--, b--, size_r--, k = *(n1 + a) - 48, l = *(n2 + b) - 48;
-    while (j >= 0)
-    {
-        i = k + l + sum;
-        if (i >= 10)
-            sum = i / 10;
-        else
-            sum = 0;
-        if (i > 0)
-        *(r + j) = (i % 10) + 48;
-        else
-            *(r + j) = '0';
-        if (a > 0)
-            a--, k = *(n1 + a) - 48;
-        else
-            k = 0;
-        if (b > 0)
-            b--, l = *(n2 + b) - 48;
-        else
-            l = 0;
-        j--, size_r--;
-    }
-    if (*(r) == '0')
-        return (r + 1);
-    else
-	    return (r);
+	while ((*(n1 + a) != '\0') && (*(n2 + b) != '\0'))
+		a++, b++;
+	if (a >= b)
+		j = a;
+	else
+		j = b;
+	if (size_r <= j + 1)
+		return (0);
+	r[j + 1] = '\0', a--, b--, size_r--, k = *(n1 + a) - 48, l = *(n2 + b) - 48;
+	while (j >= 0)
+	{
+		i = k + l + sum;
+		if (i >= 10)
+			sum = i / 10;
+		else
+			sum = 0;
+		if (i > 0)
+			*(r + j) = (i % 10) + 48;
+		else
+			*(r + j) = '0';
+		if (a > 0)
+			a--, k = *(n1 + a) - 48;
+		else
+			k = 0;
+		if (b > 0)
+			b--, l = *(n2 + b) - 48;
+		else
+			l = 0;
+		j--, size_r--;
+	}
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
