8c3d1aa49c581741be945034548392989406cba4
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-10-29 20:55:42 +0000

    Print integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_number - print an integer
+ * @n: input integer
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int k;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+		i = n;
+	j = i;
+	k = 1;
+	while (j > 9)
+	{
+		j = j /  10;
+		k = j / 10;
+	}
+	for (; k >= 1; k /= 10)
+		_putchar(((i / k) % 10) + 48);
+}
