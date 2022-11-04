1356f46fcfbd120c986bf1a5268734d4274f2186
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-04 21:20:09 +0000

    Print arguments received

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,9 +5,10 @@
  * @argv: array name
  * Return: return 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
+
 	while (i < argc)
 	{
 		printf("%s\n", argv[i]);
