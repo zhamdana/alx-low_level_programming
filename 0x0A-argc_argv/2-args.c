16603faba0a241ec2fc7b778f8b8e2d135144d57
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-04 21:16:38 +0000

    Print arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: command line arguments number
+ * @argv: array name
+ * Return: return 0
+ */
+int main(int argc, char* argv[])
+{
+	int i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
