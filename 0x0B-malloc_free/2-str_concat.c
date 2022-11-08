5ac13a5cf81932a83ebcebdd98bc98dcd784bb13
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-08 21:24:40 +0000

    Concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,25 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strdup - returns a pointer to a newly allocated space in memory.
- * @str: string.
- *
- * Return: pointer of an array of chars
+ * str_concat - concatenates two strings
+ * @s1: string
+ * @s2: string 
+ * Return: s1, s2, or null
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *a;
-	unsigned int i, j = 0;
+	char *cs;
+	unsigned int i, j = 0, k = 0;
 
-	if (str == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] || s2[i]; i++)
+		k++;
+	cs = malloc(sizeof(char) * k);
+	if (cs == NULL)
 		return (NULL);
-	for (i = 0; str[i]; i++)
-		j++;
-	a = malloc(sizeof(char) * (j + 1));
-	if (a == NULL)
-		return (NULL);
-	for (i = 0; str[i]; i++)
-		a[i] = str[i];
-	a[j] = '\0';
-	return (a);
+	for (i = 0; s1[i]; i++)
+		cs[j++] = s1[i];
+	for (i = 0; s2[i]; i++)
+		cs[j++] = s2[i];
+	return (cs);
 }
