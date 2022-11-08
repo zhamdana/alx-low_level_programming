aa41c31933d08049c062b54d7cc69a6c4c9b447e
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-08 21:16:12 +0000

    Write a function that returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strdup - return a pointer to a newly allocated space in memory
- * @str: string value
- * Return: returns array pointer
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
+ *
+ * Return: pointer of an array of chars
  */
 char *_strdup(char *str)
 {
