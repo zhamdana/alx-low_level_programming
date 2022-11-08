c7cd45dbe4e4326131ac86ae47e66c95ac305c13
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-08 21:45:27 +0000

    Return a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,28 +1,35 @@
-#include "main.h"
 #include <stdlib.h>
 /**
- * str_concat - concatenates two strings
- * @s1: string
- * @s2: string 
- * Return: s1, s2, or null
+ * alloc_grid - return a pointer to a 2 dimensional array of integers
+ * @width: array width
+ * @height: array height
+ * Return: 0, negative or NULL
  */
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-	char *cs;
-	unsigned int i, j = 0, k = 0;
+	int **i, j, k;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (i = 0; s1[i] || s2[i]; i++)
-		k++;
-	cs = malloc(sizeof(char) * k);
-	if (cs == NULL)
+	if (width < 1 || height < 1)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
-		cs[j++] = s1[i];
-	for (i = 0; s2[i]; i++)
-		cs[j++] = s2[i];
-	return (cs);
+	i = malloc(height * sizeof(int *));
+	if (i == NULL)
+	{
+		free(i);
+		return (NULL);
+	}
+	for (j = 0; j < height; j++)
+	{
+		i[j] = malloc(width * sizeof(int));
+		if (i[j] == NULL)
+		{
+			for (j--; j >= 0; j--)
+				free(i[j]);
+			free(i);
+			return (NULL);
+		}
+	}
+	for (j = 0; j < height; j++)
+		for (k = 0; k < width; k++)
+			i[j][k] = 0;
+	return (i);
 }
