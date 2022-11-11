c5d251876d04999aac8ca553af563aebcd518dea
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-11 21:17:25 +0000

    Multiply two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,10 +6,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int check_number(char *s);
-char *array_initializer(int s);
-char *zero_finder(char *s);
-int find_digit(char a);
-void find_prod(char *p, char *m, int d, int z);
-void sum_num(char *fprod, char *nprod, int nl);
 #endif
