f88e895fc63adf979ff6f3c9fb8e3d7eac2901ca
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-11 14:13:48 +0000

    Multiply two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,20 +4,20 @@
 #include <ctype.h>
 /**
  * zero_finder - find numbers that are zero
- * @vect: vector argument
+ * @argv: vector argument
  */
-void zero_finder(char *vect[])
+void zero_finder(char *argv[])
 {
 	int n, n1 = 1, n2 = 1;
 
-	for (n = 0; vect[1][n]; n++)
-		if (vect[1][n] != '0')
+	for (n = 0; argv[1][n]; n++)
+		if (argv[1][n] != '0')
 		{
 			n1 = 0;
 			break;
 		}
-	for (n = 0; vect[2][n]; n++)
-		if (vect[2][n] != '0')
+	for (n = 0; argv[2][n]; n++)
+		if (argv[2][n] != '0')
 		{
 			n2 = 0;
 			break;
@@ -45,16 +45,16 @@ char *array_initializer(char *ary, int l)
 }
 /**
  * check_number - check number length and if base 10
- * @vect: arguments vector
+ * @argv: arguments vector
  * @n: array row
  * Return: return number length
  */
-int check_number(char *vect[], int n)
+int check_number(char *argv[], int n)
 {
 	int l;
 
-	for (l = 0; vect[n][l]; l++)
-		if (!isdigit(vect[n][l]))
+	for (l = 0; argv[n][l]; l++)
+		if (!isdigit(argv[n][l]))
 		{
 			printf("Error\n");
 			exit(98);
@@ -64,47 +64,47 @@ int check_number(char *vect[], int n)
 /**
  * main - entry point for multiplying two positive numbers
  * @argc: number of arguments.
- * @vect: arguments vector.
+ * @argv: arguments vector.
  * Return: return integer
  */
-int main(int argc, char *vect[])
+int main(int argc, char *argv[])
 {
-	int l1, l2, i, sum, sumt, ii, jj, kk, ca;
+	int l1, l2, i, j, sum, sumt, x, y, z;
 	char *a;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	l1 = check_number(vect, 1), l2 = check_number(vect, 2);
-	zero_finder(vect), i = l1 + l2, a = malloc(i + 1);
+	l1 = check_number(argv, 1), l2 = check_number(argv, 2);
+	zero_finder(argv), i = l1 + l2, a = malloc(i + 1);
 	if (a == NULL)
 		printf("Error\n"), exit(98);
 	a = array_initializer(a, i);
-	kk = i - 1, ii = l1 - 1, jj = l2 - 1, ca = sumt = 0;
-	for (; kk >= 0; kk--, ii--)
+	z = i - 1, x = l1 - 1, y = l2 - 1, j = sumt = 0;
+	for (; z >= 0; z--, x--)
 	{
-		if (ii < 0)
+		if (x < 0)
 		{
 			if (sumt > 0)
 			{
-				sum = (a[kk] - '0') + sumt;
+				sum = (a[z] - '0') + sumt;
 				if (sum > 9)
-					a[kk - 1] = (sum / 10) + '0';
-				a[kk] = (sum % 10) + '0';
+					a[z - 1] = (sum / 10) + '0';
+				a[z] = (sum % 10) + '0';
 			}
-			ii = l1 - 1, jj--, sumt = 0, ca++, kk = i - (1 + ca);
+			x = l1 - 1, y--, sumt = 0, j++, z = i - (1 + j);
 		}
-		if (jj < 0)
+		if (y < 0)
 		{
 			if (a[0] != '0')
 				break;
 			i--;
 			free(a), a = malloc(i + 1), a = array_initializer(a, i);
-			kk = i - 1, ii = l1 - 1, jj = l2 - 1, ca = sumt = 0;
+			z = i - 1, x = l1 - 1, y = l2 - 1, j = sumt = 0;
 		}
-		if (jj >= 0)
+		if (y >= 0)
 		{
-			sum = ((vect[1][ii] - '0') * (vect[2][jj] - '0')) + (a[kk] - '0') + sumt;
-			sumt = sum / 10, a[kk] = (sum % 10) + '0';
+			sum = ((argv[1][x] - '0') * (argv[2][y] - '0')) + (a[z] - '0') + sumt;
+			sumt = sum / 10, a[z] = (sum % 10) + '0';
 		}
 	}
 	printf("%s\n", a);
