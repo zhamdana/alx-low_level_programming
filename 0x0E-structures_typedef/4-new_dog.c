942ad3ada1d888d218c3d94c112bb68eb79064b8
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-15 19:55:50 +0000

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,35 +10,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p_dog;
-	int i, lname, lowner;
+	dog_t *p_d;
+	int i, j, k;
 
-	p_dog = malloc(sizeof(*p_dog));
-	if (p_dog == NULL || !(name) || !(owner))
+	p_d = malloc(sizeof(*p_d));
+	if (p_d == NULL || !(name) || !(owner))
 	{
-		free(p_dog);
+		free(p_d);
 		return (NULL);
 	}
-	for (lname = 0; name[lname]; lname++)
+	for (j = 0; name[j]; j++)
 		;
-	for (lowner = 0; owner[lowner]; lowner++)
+	for (k = 0; owner[k]; k++)
 		;
-	p_dog->name = malloc(lname + 1);
-	p_dog->owner = malloc(lowner + 1);
+	p_d->name = malloc(j + 1);
+	p_d->owner = malloc(k + 1);
 
-	if (!(p_dog->name) || !(p_dog->owner))
+	if (!(p_d->name) || !(p_d->owner))
 	{
-		free(p_dog->owner);
-		free(p_dog->name);
-		free(p_dog);
+		free(p_d->owner);
+		free(p_d->name);
+		free(p_d);
 		return (NULL);
 	}
-	for (i = 0; i < lname; i++)
-		p_dog->name[i] = name[i];
-	p_dog->name[i] = '\0';
-	p_dog->age = age;
-	for (i = 0; i < lowner; i++)
-		p_dog->owner[i] = owner[i];
-	p_dog->owner[i] = '\0';
-	return (p_dog);
+	for (i = 0; i < j; i++)
+		p_d->name[i] = name[i];
+	p_d->name[i] = '\0';
+	p_d->age = age;
+	for (i = 0; i < k; i++)
+		p_d->owner[i] = owner[i];
+	p_d->owner[i] = '\0';
+	return (p_d);
 }
+
