dd2c613e32ce38092937525d876875159e7d728d
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-16 19:39:15 +0000

    Wrtie name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+  * print_name - print name
+  * @name: input name
+  * @f: finction pointer
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
