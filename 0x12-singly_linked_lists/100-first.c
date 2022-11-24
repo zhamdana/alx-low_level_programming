6571947d3265befb4451a7587fb7c53850fea938
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-24 21:29:41 +0000

    Write a function that prints a statement

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * bmain - function executed before main
+ */
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
