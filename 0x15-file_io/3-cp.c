547fdc4b97ce966169a7481fcf42cce164bb539c
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-12-05 22:43:47 +0000

    Write a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,38 +27,42 @@ void error_file(int file_from, int file_to, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int i, j, k;
-	ssize_t a, b;
+	int file_from, file_to, e_c;
+	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp i j");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	i = open(argv[1], O_RDONLY);
-	j = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(i, j, argv);
-	a = 1024;
-	while (a == 1024)
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		a = read(i, buf, 1024);
-		if (a == -1)
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
 			error_file(-1, 0, argv);
-		b = write(j, buf, a);
-		if (b == -1)
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
-	k = close(i);
-	if (k == -1)
+
+	e_c = close(file_from);
+	if (e_c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	k = close(j);
-	if (k == -1)
+
+	e_c = close(file_to);
+	if (e_c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
