949609376758c412468bed1a6ab524a09a782249
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-12-05 22:35:42 +0000

    Write a function that appends text at the end of a file.

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename
+ * @text_content: added content
+ * Return: return 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, j, k;
+
+	if (!filename)
+		return (-1);
+	i = open(filename, O_WRONLY | O_APPEND);
+	if (i == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (j = 0; text_content[j]; j++)
+			;
+		k = write(i, text_content, j);
+		if (k == -1)
+			return (-1);
+	}
+	close(i);
+	return (1);
+}
